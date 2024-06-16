5d284ee9727f4dbae793bfc65c01ae4659728c75
yeongwoo <jyy0223@naver.com>
2024-06-16 15:47:25 +0900

    [setting] project init

---
+++ src/main/java/yeongwoo/servlet/basic/HelloServlet.java
@@ -0,0 +1,28 @@
+package yeongwoo.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // header
+        response.setContentType("text/plain"); // 단순 텍스트
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
