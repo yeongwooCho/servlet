4986139696fb30f24f53015e2d6d1405e9ad3e54
yeongwoo <jyy0223@naver.com>
2024-06-25 12:52:14 +0900

    [feat] annotation 기반으로 Controller를 적용

---
+++ src/main/java/yeongwoo/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,46 @@
+package yeongwoo.servlet.web.springmvc.v3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import yeongwoo.servlet.domain.member.Member;
+import yeongwoo.servlet.domain.member.MemberRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @GetMapping("/new-form")
+    public String newForm() {
+//        return new ModelAndView("new-form");
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model
+    ) {
+        Member member = new Member(username, age);
+
+        memberRepository.save(member);
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
